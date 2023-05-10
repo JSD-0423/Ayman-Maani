9dbe8bc56cfe7413ef2990d390f425fae806c69e
AymanQ-A-Maani <maaniayman@gmail.com>
2023-05-10 12:58:34 +0300

    fixed (web topic found message and furter improvements on the media query + deleted some excess lines in the custom.css file)

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -2,8 +2,10 @@ const modesButtons = document.querySelectorAll(".modes-buttons");
 const currentScreenSize= window.visualViewport.width;
 const lastTopic = document.getElementById("last-topic");
 console.log(lastTopic)
+console.log(window.visualViewport.height)
 // const currentScreenSize= window.innerWidth;
 
+// toggle small icons for favorites and dark mode buttons in small screen///////////////////////////////////////////////////////////////
 if(currentScreenSize<= 767){
     modesButtons[0].innerHTML=`<span><ion-icon class="moon mode-icon" name="moon-outline"></ion-icon></span>`;
     modesButtons[1].innerHTML=`<span><ion-icon class="heart mode-icon" name="heart-outline"></ion-icon></span>`;
@@ -13,6 +15,11 @@ if(currentScreenSize<= 767){
     modesButtons[1].innerHTML=`<span><ion-icon class="heart mode-icon" name="heart-outline"></ion-icon></span> Favourites`;
 }
 
+modesButtons[1].addEventListener("mouseover", ()=>{
+    modesButtons[1].innerHTML=`<span><ion-icon class="heart mode-icon" name="heart"></ion-icon></span> Favourites`
+})
+
+// toggle empty div to rectify the position of web topics in small and big screens, as well as stopping it from affecting the footer distance from main section
 if(currentScreenSize>767 && currentScreenSize<1449){
     lastTopic.classList.add("d-none");
 }else if (currentScreenSize>=1449){
